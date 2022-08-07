58d89df606c18531a8da04a503f5d65a063030a7
calvinchen <calvinchen@msi.com>
2022-08-07 16:02:43 +0800

    android HW 2 1. Use NTD value for calculating USD by the fixed exchange rate currency 30.9.

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -1,14 +1,44 @@
 package com.home.currency;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ntd;
+    EditText jp;
+    EditText us;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+        ntd = findViewById (R.id.ntd);
+    }
+
+    public void currency(View view) {
+
+        String input = ntd.getText ().toString ();
+
+        if(input.isEmpty ()) {
+            new AlertDialog.Builder (this)
+                    .setTitle ("Problem")
+                    .setMessage ("Please enter NTD amount")
+                    .setPositiveButton ("OK", null)
+                    .show ();
+        } else {
+            float usd = Float.parseFloat (input);
+            usd /= 30.9;
+            new AlertDialog.Builder (this)
+                    .setTitle ("Result")
+                    .setMessage ("USD is  " + usd)
+                    .setPositiveButton ("OK", null)
+                    .show ();
+        }
+
     }
 }
